775a63956faab86148abf2c7fad2a693b8368ce5
Nicholas Peters <nicholas.peters.external@atos.net>
2024-03-20 12:25:25 +0000

    Added startup

---
+++ src/EPR.Accreditation.Facade/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace EPR.Accreditation.Facade
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IHostEnvironment env, IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
