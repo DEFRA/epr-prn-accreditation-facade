75bf6e74cec360e87e995cce51d6763a79cb2c86
Nicholas Peters <nicholas.peters.external@atos.net>
2024-03-21 10:51:44 +0000

    Changes to add endpoints for Waste Source

--- src/EPR.Accreditation.Facade/Controllers/AccreditationController.cs
+++ src/EPR.Accreditation.Facade/Controllers/AccreditationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EPR.Accreditation.Facade.Common.Dtos;
+using EPR.Accreditation.Facade.Common.RESTservices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EPR.Accreditation.Facade.Controllers
 {
@@ -6,5 +8,49 @@ namespace EPR.Accreditation.Facade.Controllers
     [Route("/api/[controller]")]
     public class AccreditationController : ControllerBase
     {
+        protected readonly IHttpAccreditationService _httpAccreditationService;
+
+        public AccreditationController(IHttpAccreditationService httpAccreditationService)
+        {
+            _httpAccreditationService = httpAccreditationService ?? throw new ArgumentNullException(nameof(httpAccreditationService));
+        }
+
+        [HttpGet("{accreditationExternalId}/Site/{siteExternalId}/Material/{materialExternalId}")]
+        public async Task<IActionResult> GetWasteSource(
+            Guid accreditationExternalId, 
+            Guid siteExternalId,
+            Guid materialExternalId)
+        {
+            var siteMaterial = await _httpAccreditationService.GetAccreditationMaterial(
+                accreditationExternalId,
+                siteExternalId,
+                materialExternalId);
+
+            if (siteMaterial == null)
+                return NotFound();
+
+            return Ok(siteMaterial.WasteSource);
+        }
+
+        [HttpPut("{accreditationExternalId}/Site/{siteExternalId}/Material/{materialExternalId}")]
+        public async Task<IActionResult> SaveWasteSource(
+            Guid accreditationExternalId,
+            Guid siteExternalId,
+            Guid materialExternalId,
+            string wasteSource)
+        {
+            var siteMaterial = new AccreditationMaterial
+            {
+                WasteSource = wasteSource
+            };
+
+            await _httpAccreditationService.UpdateAccreditationMaterial(
+                accreditationExternalId,
+                siteExternalId,
+                materialExternalId,
+                siteMaterial);
+
+            return Ok();
+        }
     }
 }
