d9793883098a771e246012e94f9a0d4eca5b32c0
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-02 09:27:13 +0100

    [feature/336347: Initial controller creation with a GET endpoint]

---
+++ src/EPR.Accreditation.Facade/Controllers/ExemptionReferenceController.cs
@@ -0,0 +1,24 @@
+﻿using EPR.Accreditation.Facade.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Accreditation.Facade.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]/{siteId}")]
+    public class ExemptionReferenceController : ControllerBase
+    {
+        protected readonly ISaveAndComeBackService _exemptionService;
+
+        public ExemptionReferenceController(IExemptionService exemeptionService)
+        {
+            _exemptionService = exemeptionService ?? throw new ArgumentNullException(nameof(exemeptionService));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetExemptionReference(int siteId)
+        {
+            var exemptionReference = await _exemptionService.GetExemptionReference(siteId);
+
+            return Ok(exemptionReference);
+        }
+    }
