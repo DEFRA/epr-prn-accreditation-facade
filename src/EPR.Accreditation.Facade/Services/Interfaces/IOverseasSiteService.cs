58cdfb0ba7406869f18224e57a15ce1a951384d3
Shehzad Ismail <shehzad.ismail@eviden.com>
2024-04-16 12:02:56 +0100

    [feature/346567: Initial facade layer WIP]

---
+++ src/EPR.Accreditation.Facade/Services/Interfaces/IOverseasSiteService.cs
@@ -0,0 +1,15 @@
+﻿using EPR.Accreditation.Facade.Common.Dtos;
+
+namespace EPR.Accreditation.Facade.Services.Interfaces
+{
+    public interface IOverseasSiteService
+    {
+        public Task<OverseasAddress> GetReprocessorDetails(
+            Guid accreditationExternalId,
+            Guid overseasSiteExternalId);
+
+        public Task UpdateReprocessorDetails(
+            Guid accreditationExternalId,
+            OverseasAddress reprocessorDetails);
+    }
+}
